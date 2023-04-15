008a2e89561de333729163bb99de5dcbadcca9ce
AVILESCASTROXAVIERANDRES <xavier.aviles07@gmail.com>
2023-04-14 21:41:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pruebainiciosesion/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.pruebainiciosesion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    //Crear 2 variables tipo editex
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Funciones
+        Funciones();
+    }
+    private fun funciones(){
+        lateini  EditText edtUsuario, edtPassword;
+
+    }
+
+}
